d03e2e75a92d721dd84160d8c07b4c7abd92cc5d
iKeirNez <keir@nellyer.co.uk>
2012-05-23 13:23:53 +0100

    Fixed interacting with blocks with an empty hand.

--- EasySlimeChunkLocator/src/me/iKeirNez/EasySlimeChunkLocator/EasySlimeChunkLocatorPlayerListener.java
+++ EasySlimeChunkLocator/src/me/iKeirNez/EasySlimeChunkLocator/EasySlimeChunkLocatorPlayerListener.java
@@ -4,7 +4,9 @@ import java.util.Random;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Chunk;
+import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
@@ -20,26 +22,29 @@ public class EasySlimeChunkLocatorPlayerListener implements Listener {
 	@EventHandler (ignoreCancelled = true)
 	public void onPlayerInteract(PlayerInteractEvent e){
 		FileConfiguration config = plugin.getConfig();
+		Player player = e.getPlayer();
 		if (e.getAction() == Action.RIGHT_CLICK_BLOCK){
-			if (e.getItem().getTypeId() == config.getInt("item")){
-				// Thanks to this for the formula
-				// http://www.minecraftforum.net/topic/397835-find-slime-spawning-chunks-125/
-				long worldSeed = e.getPlayer().getWorld().getSeed();
-			    Chunk playerChunk = e.getPlayer().getWorld().getChunkAt(e.getClickedBlock());
-			    int x = playerChunk.getX();
-			    int z = playerChunk.getZ();
+			if (player.getItemInHand().getType() != Material.AIR){
+				if (e.getItem().getTypeId() == config.getInt("item")){
+					// Thanks to this for the formula
+					// http://www.minecraftforum.net/topic/397835-find-slime-spawning-chunks-125/
+					long worldSeed = player.getWorld().getSeed();
+				    Chunk playerChunk = player.getWorld().getChunkAt(e.getClickedBlock());
+				    int x = playerChunk.getX();
+				    int z = playerChunk.getZ();
 
-			    Random random = new Random(worldSeed + 
-			    x * x * 4987142 + 
-			    x * 5947611 + 
-			    z * z * 4392871L + 
-			    z * 389711 ^ 0x3AD8025F);
-			      
-			    if (random.nextInt(10) == 0){
-			    	e.getPlayer().sendMessage(ChatColor.GREEN + config.getString("Messages.PartOfSlimeChunk"));
-			    } else {
-			    	e.getPlayer().sendMessage(ChatColor.RED + config.getString("Messages.NotPartOfSlimeChunk"));
-			    }
+				    Random random = new Random(worldSeed + 
+				    x * x * 4987142 + 
+				    x * 5947611 + 
+				    z * z * 4392871L + 
+				    z * 389711 ^ 0x3AD8025F);
+				      
+				    if (random.nextInt(10) == 0){
+				    	player.sendMessage(ChatColor.GREEN + config.getString("Messages.PartOfSlimeChunk"));
+				    } else {
+				    	player.sendMessage(ChatColor.RED + config.getString("Messages.NotPartOfSlimeChunk"));
+				    }
+				}
 			}
 		}
 	}
